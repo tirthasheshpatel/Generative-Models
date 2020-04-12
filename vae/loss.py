bc30040c951e3a86fa81b37db15684fea7d98039
tirthasheshpatel <tirthasheshpatel@gmail.com>
2020-04-12 17:15:09 +0530

    move from normal to binomial pdf

--- vae/loss.py
+++ vae/loss.py
@@ -9,8 +9,8 @@ class BinomialVLB(object):
     @staticmethod
     def unsigned_call(x, x_recon, loc, log_var):
         KL_loss = - 0.5 * tf.reduce_sum( -log_var + tf.math.exp( log_var ) + tf.square( loc ), axis=-1 )
-        cross_entropy_loss = tf.reduce_sum( x * tf.math.log( x_recon ) + (1-x) * tf.math.log( 1-x_recon ), axis=[-3, -2, -1] )
-        vlb = tf.reduce_mean(cross_entropy_loss - KL_loss)
+        cross_entropy_loss = tf.reduce_sum( x * tf.math.log( x_recon+1e-10 ) + (1-x) * tf.math.log( 1-x_recon+1e-10 ), axis=[-3, -2, -1] )
+        vlb = tf.reduce_mean(cross_entropy_loss + KL_loss)
         return vlb
 
 class NormalVLB(object):
