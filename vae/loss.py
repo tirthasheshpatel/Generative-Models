12b288514a51b036e7fa98265ec53746b745b85f
Tirth Patel <tirthasheshpatel@gmail.com>
2020-04-13 07:10:40 +0530

    add the latent_dims term in KL loss

--- vae/loss.py
+++ vae/loss.py
@@ -8,7 +8,7 @@ class BinomialVLB(object):
 
     @staticmethod
     def unsigned_call(x, x_recon, loc, log_var):
-        KL_loss = - 0.5 * tf.reduce_sum( -log_var + tf.math.exp( log_var ) + tf.square( loc ), axis=-1 )
+        KL_loss = - 0.5 * tf.reduce_sum( -log_var + tf.math.exp( log_var ) + tf.square( loc ) - 1, axis=-1 )
         cross_entropy_loss = tf.reduce_sum( x * tf.math.log( x_recon+1e-10 ) + (1-x) * tf.math.log( 1-x_recon+1e-10 ), axis=[-3, -2, -1] )
         vlb = tf.reduce_mean(cross_entropy_loss + KL_loss)
         return vlb
@@ -19,7 +19,7 @@ class NormalVLB(object):
 
     @staticmethod
     def unsigned_call(x, x_recon, loc, log_var):
-        KL_loss = - 0.5 * tf.reduce_sum( -log_var + tf.math.exp( log_var ) + tf.square( loc ), axis=-1 )
+        KL_loss = - 0.5 * tf.reduce_sum( -log_var + tf.math.exp( log_var ) + tf.square( loc ) - 1, axis=-1 )
         reconstruction_loss = tf.reduce_sum( tf.square( x - x_recon ), axis=[-3, -2, -1] )
         vlb = tf.reduce_mean(reconstruction_loss + KL_loss)
         return vlb
