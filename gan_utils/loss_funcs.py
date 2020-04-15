f2ae608149f719fce8c4081cee295bd5a6ca45a1
tirthasheshpatel <tirthasheshpatel@gmail.com>
2020-04-16 00:03:09 +0530

    fix loss reduce axis

--- gan_utils/loss_funcs.py
+++ gan_utils/loss_funcs.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 
 def discriminator_loss(true_labels, gen_labels):
-    loss = tf.reduce_sum(tf.log(true_labels) + tf.log(1 - gen_labels), axis=[-3, -2, -1])
+    loss = tf.reduce_sum(tf.log(true_labels) + tf.log(1 - gen_labels), axis=-1)
     return -tf.reduce_mean(loss)
 
 def generator_loss(gen_labels):
-    loss = tf.reduce_sum(tf.log(gen_labels), axis=[-3, -2, -1])
+    loss = tf.reduce_sum(tf.log(gen_labels), axis=-1)
     return -tf.reduce_mean(loss)
