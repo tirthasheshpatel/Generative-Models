5f6dcff0d4c82d86460d463f1998215bcb27198a
tirthasheshpatel <tirthasheshpatel@gmail.com>
2020-04-15 23:24:57 +0530

    add utils for dcgan

---
+++ gan_utils/gan_discriminator.py
@@ -0,0 +1,24 @@
+import os
+
+import keras
+
+IMG_SIZE = int(os.getenv('IMG_SIZE'))
+
+def GANDiscriminator():
+    model = keras.applications.InceptionV3(include_top=False,
+                                           input_shape=(IMG_SIZE, IMG_SIZE, 3),
+                                           weights='imagenet')
+    new_output = keras.layers.GlobalAveragePooling2D()(model.output)
+    new_output = keras.layers.Dense(1, activation='sigmoid')(new_output)
+    model = keras.engine.training.Model(model.inputs, new_output)
+
+    for layer in model.layers:
+        layer.trainable = True
+        if isinstance(layer, keras.layers.BatchNormalization):
+            layer.momentum = 0.9
+
+    for layer in model.layers[:-50]:
+        if not isinstance(layer, keras.layers.BatchNormalization):
+            layer.trainable = False
+
+    return model
---
+++ gan_utils/gan_generator.py
@@ -0,0 +1,52 @@
+import os
+
+import keras
+
+LATENT_DIMS = int(os.getenv('LATENT_DIMS'))
+IMG_SIZE = int(os.getenv('IMG_SIZE'))
+
+def GANGenerator():
+    model = keras.models.Sequential(name='generator')
+    model.add(keras.layers.InputLayer(input_shape=[LATENT_DIMS]))
+    model.add(keras.layers.Conv2DTranspose(filters=1024,
+                                           kernel_size=8,
+                                           strides=1,
+                                           padding='VALID',
+                                           data_format='channels_last',
+                                           activation='relu'))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2DTranspose(filters=512,
+                                           kernel_size=3,
+                                           strides=2,
+                                           padding='SAME',
+                                           data_format='channels_last',
+                                           activation='relu'))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2DTranspose(filters=256,
+                                           kernel_size=3,
+                                           strides=2,
+                                           padding='SAME',
+                                           data_format='channels_last',
+                                           activation='relu'))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2DTranspose(filters=128,
+                                           kernel_size=3,
+                                           strides=2,
+                                           padding='SAME',
+                                           data_format='channels_last',
+                                           activation='relu'))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2DTranspose(filters=64,
+                                           kernel_size=3,
+                                           strides=2,
+                                           padding='SAME',
+                                           data_format='channels_last',
+                                           activation='relu'))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Conv2DTranspose(filters=3,
+                                           kernel_size=3,
+                                           strides=2,
+                                           padding='SAME',
+                                           data_format='channels_last',
+                                           activation='relu'))
+    return model
