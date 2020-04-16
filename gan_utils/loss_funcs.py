8134cd447337bdcde01935ee538bef748227906f
tirthasheshpatel <tirthasheshpatel@gmail.com>
2020-04-16 11:12:32 +0530

    reduce noise in loss funcs

--- gan_utils/loss_funcs.py
+++ gan_utils/loss_funcs.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 
 def discriminator_loss(true_labels, gen_labels):
-    loss = tf.reduce_mean(tf.log(true_labels + 1e-4) + tf.log(1 - gen_labels + 1e-4))
+    loss = tf.reduce_mean(tf.log(true_labels + 1e-9) + tf.log(1 - gen_labels + 1e-9))
     return -loss
 
 def generator_loss(gen_labels):
-    loss = tf.reduce_mean(tf.log(gen_labels + 1e-8))
+    loss = tf.reduce_mean(tf.log(gen_labels))
     return -loss
