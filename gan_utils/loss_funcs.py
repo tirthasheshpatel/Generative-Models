406cd4a86db2367a695e5a804b1594091d44e46e
tirthasheshpatel <tirthasheshpatel@gmail.com>
2020-04-16 10:04:21 +0530

    remove redundant operations from loss functions

--- gan_utils/loss_funcs.py
+++ gan_utils/loss_funcs.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 
 def discriminator_loss(true_labels, gen_labels):
-    loss = tf.reduce_sum(tf.log(true_labels + 1e-19) + tf.log(1 - gen_labels + 1e-19), axis=-1)
-    return -tf.reduce_mean(loss)
+    loss = tf.reduce_mean(tf.log(true_labels + 1e-8) + tf.log(1 - gen_labels + 1e-8))
+    return -loss
 
 def generator_loss(gen_labels):
-    loss = tf.reduce_sum(tf.log(gen_labels + 1e-19), axis=-1)
-    return -tf.reduce_mean(loss)
+    loss = tf.reduce_mean(tf.log(gen_labels + 1e-8))
+    return -loss
